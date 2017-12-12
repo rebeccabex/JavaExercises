02c1b14051aa71565948ff52ab30779468ab196a
rebeccabex <rebeccabexb@gmail.com>
2017-12-12 10:57:51 +0000

    Improved PaintWizard

--- Exercises/src/main/java/IntermediateExercises.java
+++ Exercises/src/main/java/IntermediateExercises.java
@@ -160,55 +160,13 @@ public class IntermediateExercises {
 
         System.out.println("\nPaint Wizard Exercises");
 
-        ArrayList<Paint> paintList = new ArrayList<Paint>();
+        PaintWizard paintWizard = new PaintWizard();
 
-        Paint cheapoMax = new Paint("CheapoMax", 20, 19.99, 10);
-        Paint averageJoes = new Paint("AverageJoes", 15, 17.99, 11);
-        Paint duluxourousPaints = new Paint("DuluxourousPaints", 10, 25, 20);
+        System.out.println(paintWizard.cheapestPaint(100).getReceipt(100));
+        System.out.println(paintWizard.cheapestPaint(100).getReceipt(1000));
 
-        paintList.add(cheapoMax);
-        paintList.add(averageJoes);
-        paintList.add(duluxourousPaints);
-
-        System.out.println(cheapestPaint(paintList,100).getReceipt(100));
-        System.out.println(cheapestPaint(paintList,100).getReceipt(1000));
-
-        System.out.println(leastWaste(paintList,100).getReceipt(100));
-        System.out.println(leastWaste(paintList,100).getReceipt(1000));
-
-    }
-
-    private Paint cheapestPaint(ArrayList<Paint> paintList, int roomSize) {
-
-        double lowestCost = -1;
-        Paint bestValue = null;
-
-        for(Paint p : paintList) {
-            double cost = p.costToPaint(roomSize);
-            if (lowestCost == -1 || cost < lowestCost) {
-                lowestCost = cost;
-                bestValue = p;
-            }
-        }
-
-        return bestValue;
-    }
-
-    private Paint leastWaste(ArrayList<Paint> paintList, int roomSize) {
-
-        int lowestWaste = -1;
-        Paint bestPaint = null;
-
-
-        for(Paint p : paintList) {
-            int waste = p.wastedPaint(roomSize);
-            if (lowestWaste == -1 || waste < lowestWaste) {
-                lowestWaste = waste;
-                bestPaint = p;
-            }
-        }
-
-        return bestPaint;
+        System.out.println(paintWizard.leastWaste(100).getReceipt(100));
+        System.out.println(paintWizard.leastWaste(100).getReceipt(1000));
 
     }
 
