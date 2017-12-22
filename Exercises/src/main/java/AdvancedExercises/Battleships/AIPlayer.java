36d942aae38afd65b46c2c9c4c6f0c236643ef0b
rebeccabex <rebeccabexb@gmail.com>
2017-12-22 11:32:50 +0000

    Part way through improving AI in Battleships

--- Exercises/src/main/java/AdvancedExercises/Battleships/AIPlayer.java
+++ Exercises/src/main/java/AdvancedExercises/Battleships/AIPlayer.java
@@ -2,12 +2,17 @@ package AdvancedExercises.Battleships;
 
 import AdventureGame.RandomGenerator;
 
+import java.util.ArrayList;
+
 public class AIPlayer extends Player {
 
     RandomGenerator randGen = new RandomGenerator();
+    int[] target;
+    ArrayList<int[]> targetArea;
 
     public AIPlayer(int playerNo, String name, int gridSize) {
         super(playerNo, name, gridSize);
+        target = resetTarget();
     }
 
     @Override
@@ -40,17 +45,29 @@ public class AIPlayer extends Player {
     @Override
     public int[] takeTurn() {
 
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            System.out.println("Failed to sleep");
+//        }
+
+        int[] playerGuess;
+
+        if (target[0] == -1) {
+            playerGuess = randomGuess();
+        } else {
+            playerGuess = targettedGuess();
+        }
+
+        return playerGuess;
+    }
+
+    private int[] randomGuess() {
+
         boolean validGuess = false;
         int[] playerGuess = {-1, -1};
 
         while (!validGuess) {
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println("Failed to sleep");
-            }
-
             playerGuess[0] = randGen.getRandomInt(targetGrid.getSize());
             playerGuess[1] = randGen.getRandomInt(targetGrid.getSize());
 
@@ -60,7 +77,75 @@ public class AIPlayer extends Player {
                 validGuess = true;
             }
         }
-
         return playerGuess;
+    }
+
+    private int[] targettedGuess() {
+
+        boolean validGuess = false;
+        int[] playerGuess = {-1, -1};
+
+        while (!validGuess) {
+
+            int randNum = randGen.getRandomInt(targetArea.size());
+            playerGuess = targetArea.get(randNum);
+
+            int spaceVal = targetGrid.targetCoordinates(playerGuess[0], playerGuess[1]);
+
+            if (spaceVal == 0) {
+                validGuess = true;
+            } else {
+                targetArea.remove(randNum);
+                if (targetArea.isEmpty()) {
+                    playerGuess = randomGuess();
+                    validGuess = true;
+                    target = resetTarget();
+                }
+            }
+        }
+        return playerGuess;
+    }
+
+    @Override
+    public void shotOutcome(int[] inputCoord, int outcome) {
+        super.shotOutcome(inputCoord, outcome);
+        if (outcome != 0) {
+            target = inputCoord;
+            setTargetArea(target);
+        } else {
+            target = resetTarget();
+        }
+    }
+
+    private int[] resetTarget() {
+
+        int[] blankArray = {-1, -1};
+
+        for (int i = 0; i < 2; i++) {
+            blankArray[i] = -1;
+        }
+
+        return blankArray;
+    }
+
+    // Need to correct targetArea
+    private void setTargetArea(int[] target) {
+
+
+        int[][] plusMinus = {{-1, 0} ,{1, 0} ,{0, -1} ,{0, 1}};
+
+
+        for (int i = 0; i < 4; i++) {
+
+
+
+//            int[] tempArray = {target[0], target[1]};
+//            for (int j = 0; j < 2; j++) {
+//                tempArray[j] = target[j] + plusMinus[i];
+//                if (tempArray[0] >= 0 || tempArray[0] < targetGrid.getSize() || tempArray[1] >= 0 || tempArray[1] < targetGrid.getSize()) {
+//                    targetArrayList.add(tempArray);
+//                }
+//            }
+        }
     }
 }
